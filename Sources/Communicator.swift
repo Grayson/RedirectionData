ebe75208797a4c20cdcccbd19861f3b471a3c9c9
Grayson <info@fromconcentratesoftware.com>
2017-04-25 20:33:08 -0400

    Define the `Communicator` protocol for interapp communication

---
+++ Sources/Communicator.swift
@@ -0,0 +1,16 @@
+//
+//  Communicator.swift
+//  RedirectionData
+//
+//  Created by Grayson Hansard on 4/25/17.
+//
+//
+
+import Foundation
+
+@objc protocol Communicator {
+	func ping(_ pong: () -> ())
+	func handle(message: InterappMessage)
+	func update(rules: [EncodableRule])
+	func log(messages: [EncodableLogMessage])
+}
